0d6597c5359264cdda824bdda3bcb3267ae321cb
Danny Soto <87dsoto@gmail.com>
2020-01-23 21:55:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |name, data|
     if name == "Freddy Mercury"
-      name.each do |key, val|
+      data.each do |key, val|
         if key == :favorite_ice_cream_flavors
         val.delete_if {|flavor| flavor == "strawberry"}
         end
